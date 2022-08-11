4b06936edaa0e35b2ac2cde03076bb59da17da77
jkol7 <jkolcun7@gmail.com>
2022-08-11 16:41:40 -0400

    Deploy

--- server/server.js
+++ server/server.js
@@ -22,7 +22,6 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = process.env.CONNECTION_URL
 
 
 //HTTP request logger
@@ -41,7 +40,7 @@ if (process.env.NODE_ENV === 'production'){
 
 console.log('Check 2')
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
 .catch((error) => console.log(error.message) )
 
