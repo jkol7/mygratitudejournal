aeef233fa9d6266579604ad3ae5fd37f19762697
jkol7 <jkolcun7@gmail.com>
2022-08-12 11:45:20 -0400

    Added authorization hash

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import asyncHandler from 'express-async-handler'
 import {User} from '../models/userModel.js'
+import mongoose from 'mongoose'
 
 // @desc    Register user
 // @route   POST /api/users
@@ -13,6 +14,40 @@ const registerUser = asyncHandler (async (req, res) => {
     if (!name || !email || !password){
         res.status(400)
         throw new Error('Please add all fields')
+    }
+
+    // Check if user exists
+
+    const userExists = await User.findOne({email})
+
+    if (userExists){
+        res.status(400)
+        throw new Error('User already exists')
+
+    }
+
+    // Hash password
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
     }
 
     res.json({message: 'Register User'})
