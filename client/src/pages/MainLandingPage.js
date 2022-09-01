7f70955a818686f946c616c71189ff00b43820c1
jkol7 <jkolcun7@gmail.com>
2022-08-31 20:30:10 -0400

    Button style

--- client/src/pages/MainLandingPage.js
+++ client/src/pages/MainLandingPage.js
@@ -5,9 +5,9 @@ function MainLandingPage() {
   return (
         <section className='main-lander-inner-bg'>
             <div className='main-lander-top-left'>  
-                <h2>Grow Your Gratitude</h2>
-                <h4>free personal journal</h4>
-                <button className='buttonsection'>Start Now</button>
+                <h2>Free Personal Journal</h2>
+                <h4>experience the power of gratitudes</h4>
+                <button className='landing-top-button'>Start Now</button>
             </div>  
             <div className='main-lander-top-right'>
                 <img src='landingpage-right.png'/>
