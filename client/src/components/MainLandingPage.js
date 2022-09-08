b023627100132bbc50646b97c46bb71bb43dbaf5
jkol7 <jkolcun7@gmail.com>
2022-09-08 17:18:25 -0400

    Updated typing animation on landing page and footer

--- client/src/components/MainLandingPage.js
+++ client/src/components/MainLandingPage.js
@@ -5,9 +5,9 @@ function MainLandingPage() {
   return (
     <section className="main-lander-inner-bg">
       <div className="main-lander-wrapper">
-        <div className="main-lander-top-left">
+        <div className="main-lander-top-left flex-col">
           <h2>Free Personal Journal</h2>
-          <h4>experience the power of gratitudes</h4>
+          <h4>experience the power of gratitude</h4>
           <Link to="/register">
             <button className="landing-top-button">Start Now</button>
           </Link>
@@ -16,9 +16,9 @@ function MainLandingPage() {
           <img src="landingpage-right.png" />
         </div>
       </div>
-      <div className="main-lander-benefits">
+      <div className="main-lander-benefits flex-col">
         <div className="benefit-container">
-          <div className="benefit-text-container">
+          <div className="flex-col">
             <img src="mentalhealth.svg" />
             <h4>Improve mental health</h4>
             <p>
@@ -26,7 +26,7 @@ function MainLandingPage() {
               well-being and less anxiety.
             </p>
           </div>
-          <div className="benefit-text-container">
+          <div className="flex-col">
             <img src="relationships.svg" />
             <h4>Build strong relationships</h4>
             <p>
@@ -34,7 +34,7 @@ function MainLandingPage() {
               satisfaction.
             </p>
           </div>
-          <div className="benefit-text-container">
+          <div className="flex-col">
             <img src="sleep.svg" />
             <h4>Sleep better and longer</h4>
             <p>
@@ -43,6 +43,19 @@ function MainLandingPage() {
           </div>
         </div>
       </div>
+      <div className="flex-col prompt-landing-section">
+        <h4>INSPIRATIONAL PROMPTS</h4>
+        <h1 className="typewriter">What made you laugh or smile recently?</h1>
+        <p>Whenever you lack inspiration, your journal is here to help!</p>
+        <Link to="/register">
+          <button className="landing-bottom-button">Start Now</button>
+        </Link>
+      </div>
+      <footer>
+        <p>© 2022 GJjournal.com</p>
+        <p>Terms of Service</p>
+        <p>Privacy Policy</p>
+      </footer>
     </section>
   );
 }
